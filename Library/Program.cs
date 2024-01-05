c6a8030ed83070f47f649452843a7ec3b5997614
Jakub Tomiczek <tomiczekkuba01@gmail.com>
2024-01-05 18:16:36 +0100

    Configure startup file

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,6 +1,52 @@
+// using Library.Areas.Identity.Data;
+// using Library.Models;
+// using Microsoft.EntityFrameworkCore;
+//
+// var builder = WebApplication.CreateBuilder(args);
+// var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ??
+//                        throw new InvalidOperationException(
+//                            "Connection string 'ApplicationDbContextConnection' not found.");
+//
+// builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//     options.UseSqlServer(connectionString));
+//
+// builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+//     .AddEntityFrameworkStores<ApplicationDbContext>();
+//
+// // Add services to the container.
+// builder.Services.AddControllersWithViews();
+//
+// var app = builder.Build();
+//
+// // Configure the HTTP request pipeline.
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+//
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
+//
+// app.UseRouting();
+// app.UseAuthentication();
+// ;
+//
+// app.UseAuthorization();
+//
+// app.MapControllerRoute(
+//     "default",
+//     "{controller=Home}/{action=Index}/{id?}");
+//
+// app.MapRazorPages();
+//
+// app.Run();
+
 using Library.Areas.Identity.Data;
 using Library.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ??
@@ -31,8 +77,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 app.UseAuthentication();
-;
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -40,5 +84,10 @@ app.MapControllerRoute(
     "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
+
+// Additional configuration for API CRUD
+app.MapControllerRoute(
+    name: "api",
+    pattern: "api/{controller=Books}/{action=Index}/{id?}");
 
 app.Run();
